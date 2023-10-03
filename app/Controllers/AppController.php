e8dc14fae6d37ee3eac4b99939ee2738adb4e980
roman-kozii <roman.business0115@outlook.com>
2023-10-03 14:01:41 -0600

    fix: restore dashboard

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -8,9 +8,16 @@ use StellarRouter\{Get, Group};
 #[Group(middleware: ["auth"])]
 class AppController extends Controller
 {
-    #[Get("/app", "app.index", ["push-url"])]
+    #[Get("/dashboard", "dashboard.index", ["push-url"])]
     public function index(): string
     {
-        return latte("app/index.latte");
+        return latte("dashboard/index.latte");
     }
+
+    // Svelte app
+    // #[Get("/app", "app.index", ["push-url"])]
+    // public function index(): string
+    // {
+    //     return latte("app/index.latte");
+    // }
 }
