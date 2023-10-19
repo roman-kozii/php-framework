9917103d6409c9a7a31a63f69874cfe48b011b5f
roman-kozii <roman.business0115@outlook.com>
2023-10-18 19:02:33 -0600

    fix: module changes
    - Bold fold search label

--- app/Modules/Test.php
+++ app/Modules/Test.php
@@ -8,32 +8,38 @@ class Test extends Module
 {
     public function __construct()
     {
+        // Table columns for Index view
         $this->table_columns = [
             "id" => "ID",
             "number" => "Number",
             "name" => "Name",
         ];
 
+        // Form columns for Edit / Create views
         $this->form_columns = [
             "number" => "Number",
             "name" => "Name",
             "comment" => "Comment",
         ];
 
+        // Form valdiation columns / rules
         $this->validation = [
             "name" => ["required"],
             "number" => ["required", "numeric"],
         ];
 
+        // Edit / Create view controls
         $this->form_controls = [
             "number" => "text",
             "name" => "text",
             "comment" => "textarea",
         ];
 
+        // Searchable columns
         $this->search = [
             "number",
-            "name"
+            "name",
+            "comment"
         ];
 
         parent::__construct("test", "test");
