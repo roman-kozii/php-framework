64d0dc4de155f50211c7b87b148e5acfa1634ea0
roman-kozii <roman.business0115@outlook.com>
2023-10-21 16:05:12 -0600

    fix: update scripts

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -2,13 +2,15 @@
 feather.replace();
 
 // Sidebar active toggle
-const sidebar_links = document.querySelectorAll('#sidebar .nav-pills li.nav-item');
-sidebar_links.forEach((link) => {
-  link.addEventListener('click', function(e) {
-    // Remove active from all sidebar links
-    sidebar_links.forEach((l) => l.classList.remove("active"));
-    const target = e.currentTarget;
-    // Add active to the clicked element
-    target.classList.add('active');
+if (typeof sidebar_links === 'undefined') {
+  const sidebar_links = document.querySelectorAll('#sidebar .nav-pills li.nav-item');
+  sidebar_links.forEach((link) => {
+    link.addEventListener('click', function(e) {
+      // Remove active from all sidebar links
+      sidebar_links.forEach((l) => l.classList.remove("active"));
+      const target = e.currentTarget;
+      // Add active to the clicked element
+      target.classList.add('active');
+    });
   });
-});
+}
