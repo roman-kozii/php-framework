d8a4e2deb44451dbbfec89572fb0ffb40361f145
roman-kozii <roman.business0115@outlook.com>
2023-10-27 11:02:44 -0600

    fix: tweak profiler

--- public/assets/js/module.js
+++ public/assets/js/module.js
@@ -3,3 +3,17 @@ function toggleProfiler(e)
   const div = e.currentTarget.nextElementSibling;
   div.classList.toggle("hidden");
 }
+
+if (typeof sidebar_links === 'undefined') {
+  // Sidebar active toggle
+  const sidebar_links = document.querySelectorAll('#sidebar .nav-pills li.nav-item');
+  sidebar_links.forEach((link) => {
+    link.addEventListener('click', function(e) {
+      // Remove active from all sidebar links
+      sidebar_links.forEach((l) => l.classList.remove("active"));
+      const target = e.currentTarget;
+      // Add active to the clicked element
+      target.classList.add('active');
+    });
+  });
+}
