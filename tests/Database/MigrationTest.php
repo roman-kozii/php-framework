38e83c36b6e9f82a2471f43b1d4f568b2912b438
roman-kozii <roman.business0115@outlook.com>
2023-11-10 14:20:07 -0700

    fix: test

--- tests/Database/MigrationTest.php
+++ tests/Database/MigrationTest.php
@@ -19,7 +19,7 @@ final class MigrationTest extends TestCase
     public function test_migration_up_query(): void
     {
         $migration = $this->migration->up();
-        $query = "CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, uuid CHAR(36) NOT NULL DEFAULT (UUID()), name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password BINARY(96) NOT NULL, two_fa_secret CHAR(16), remember_token CHAR(64), reset_token CHAR(64), reset_expires_at BIGINT UNSIGNED, failed_login_attempts TINYINT UNSIGNED NOT NULL DEFAULT 0, lock_expires_at BIGINT UNSIGNED, created_at DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP, UNIQUE KEY (email), PRIMARY KEY (id))";
+        $query = "CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, uuid CHAR(36) NOT NULL DEFAULT (UUID()), user_type BIGINT UNSIGNED NOT NULL DEFAULT 2, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password BINARY(96) NOT NULL, two_fa_secret CHAR(16), remember_token CHAR(64), reset_token CHAR(64), reset_expires_at BIGINT UNSIGNED, failed_login_attempts TINYINT UNSIGNED NOT NULL DEFAULT 0, lock_expires_at BIGINT UNSIGNED, created_at DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP, UNIQUE KEY (email), PRIMARY KEY (id), FOREIGN KEY (user_type) REFERENCES user_types(id))";
         $this->assertSame($query, $migration);
     }
 
