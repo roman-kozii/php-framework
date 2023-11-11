b007f7d3eb43575d04557a78142975eb59bbd5c7
roman-kozii <roman.business0115@outlook.com>
2023-11-10 19:14:16 -0700

    fix: audit super admin only

--- migrations/1699639640_insert_modules.php
+++ migrations/1699639640_insert_modules.php
@@ -9,9 +9,13 @@ return new class implements Migration
 {
     public function up(): string
     {
+            // Note: user_types
+            // 1: super admin
+            // 2: admin
+            // 3: standard
             return Schema::raw("INSERT INTO modules (module_name, class_name, module_table, module_title, module_icon, user_type) VALUES
                 ('home', 'App\\\\Modules\\\\Home', null, 'Home', 'home', 3),
-                ('audit', 'App\\\\Modules\\\\Audit', 'audit', 'Audit', 'flag', 2),
+                ('audit', 'App\\\\Modules\\\\Audit', 'audit', 'Audit', 'flag', 1),
                 ('modules', 'App\\\\Modules\\\\Modules', 'modules', 'Modules', 'cpu', 1),
                 ('sessions', 'App\\\\Modules\\\\Sessions', 'sessions', 'Sessions', 'activity', 2),
                 ('users', 'App\\\\Modules\\\\Users', 'users', 'Users', 'users', 2)");
