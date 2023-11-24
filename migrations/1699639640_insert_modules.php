ff1185f0b4acd88671ec0ec00edbd3708601c7b1
roman-kozii <roman.business0115@outlook.com>
2023-11-24 14:40:11 -0700

    fix: users super admin

--- migrations/1699639640_insert_modules.php
+++ migrations/1699639640_insert_modules.php
@@ -14,12 +14,12 @@ return new class implements Migration
             // 2: admin
             // 3: standard
             return Schema::raw("INSERT INTO modules (module_name, class_name, module_table, module_title, module_icon, user_type) VALUES
-                ('home', 'App\\\\Modules\\\\Home', null, 'Home', 'home', 3),
+                ('users', 'App\\\\Modules\\\\Users', 'users', 'Users', 'users', 1),
                 ('audit', 'App\\\\Modules\\\\Audit', 'audit', 'Audit', 'flag', 1),
                 ('modules', 'App\\\\Modules\\\\Modules', 'modules', 'Modules', 'cpu', 1),
-                ('blog', 'App\\\\Modules\\\\Blog', 'posts', 'Blog', 'feather', 1),
+                ('blog', 'App\\\\Modules\\\\Blog', 'posts', 'Blog', 'feather', 2),
                 ('sessions', 'App\\\\Modules\\\\Sessions', 'sessions', 'Sessions', 'activity', 2),
-                ('users', 'App\\\\Modules\\\\Users', 'users', 'Users', 'users', 2)
+                ('home', 'App\\\\Modules\\\\Home', null, 'Home', 'home', 3),
                 ");
     }
 
