6d9a6ad219c431bb7d6db895ef27295a7a2bd23c
roman-kozii <roman.business0115@outlook.com>
2023-11-25 09:42:21 -0700

    refactor: templates, wip

--- public/assets/js/module.js
+++ public/assets/js/module.js
@@ -3,20 +3,7 @@ function toggleProfiler(e) {
   div.classList.toggle("hidden");
 }
 
-function handleSidebarActive(e) {
-  const el = e.currentTarget;
-  const sidebar_links = document.querySelectorAll('#sidebar .nav-pills li.nav-item');
-  sidebar_links.forEach((l) => {
-    if (l.dataset.module === el.dataset.module) {
-      l.classList.add("active")
-    } else {
-      l.classList.remove("active")
-    }
-  });
-}
-
 function disablePaginationLink(e) {
   e.preventDefault();
   e.currentTarget.parentElement.classList.add("disabled");
 }
-
--- public/assets/js/scripts.js
+++
@@ -1,2 +0,0 @@
-// Feather icons
-feather.replace();
